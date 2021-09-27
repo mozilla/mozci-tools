20094f8e8dba60e8fe41f7e185a699bd6c0f7986
Andrew Halberstadt <ahal@pm.me>
2021-09-27 17:14:03 -0400

    Create project scaffolding and pyproject.toml

---
+++ test_management_tools/dump_failures.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+"""This script will display information about failing tests given a push.
+
+Usage:
+
+    $ pip install -r requirements.txt
+    $ ./dump-failures try <revision>
+
+To automatically open failures in vim you can set:
+
+    $ export DUMP_FAILURES_EDIT_COMMAND="vim +/{test} +:vnew +':let f=append(1,{context})' /path/to/mozilla-central/{manifest}"
+"""
+
+import json
+import os
+import shlex
+import subprocess
+import sys
+from argparse import ArgumentParser
+from collections import defaultdict
+
+from mozci.util.logging import logger
+from mozci.push import Push
+from mozci.task import TestTask
+
+RESULTS = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: [0, 0])))
+
+
+def update_results(task):
+    raw_log_path = [a for a in task.artifacts if a.endswith("raw.log")][0]
+    raw_log = task.get_artifact(raw_log_path).text
+    manifests = {}
+    status = defaultdict(int)
+    seen = set()
+    for line in raw_log.splitlines():
+        data = json.loads(line)
+        if data["action"] == "suite_start":
+            for manifest, tests in data["tests"].items():
+                for t in tests:
+                    manifests[t] = manifest
+
+        if (
+            data["action"] not in ("test_status", "test_end", "crash")
+            or "test" not in data
+        ):
+            continue
+
+        test = data["test"]
+
+        if (
+            task.classification == "not classified"
+            and (data["action"] == "crash"
+            or ("expected" in data and data["status"] != data["expected"]))
+        ):
+            status[test] |= 1
+
+        if test not in seen and data["action"] in ("crash", "test_end"):
+            try:
+                manifest = manifests[test]
+            except KeyError:
+                try:
+                    manifest = manifests[f"browser/{test}"]
+                except KeyError:
+                    logger.warning(f"Invalid test: {test}")
+                    continue
+
+            seen.add(test)
+            result = RESULTS[manifest][test][task.label]
+            result[status[test]] += 1
+
+
+def dump_failures(branch, rev):
+    push = Push(rev, branch)
+    tasks = [t for t in push.tasks if isinstance(t, TestTask)]
+    for task in tasks:
+        logger.info(f"processing {task.label}")
+        update_results(task)
+
+    for manifest, tests in RESULTS.items():
+        manifest_printed = False
+
+        for test, tasks in tests.items():
+            if not any(r[1] for r in tasks.values()):
+                continue
+
+            if not manifest_printed:
+                print("\n" + manifest)
+                manifest_printed = True
+
+            print()
+            print(f"  {test}")
+            context = []
+            for label, result in tasks.items():
+                if not result[1]:
+                    continue
+                s = f"    {label}: {result[1]} / {result[0] + result[1]} failed"
+                context.append(s)
+                print(s)
+
+            if manifest in ("dom/canvas/test/mochitest.ini",):
+                continue
+
+            if "DUMP_FAILURES_EDIT_COMMAND" in os.environ:
+                context.insert(0, test)
+                context = [l.strip() for l in context]
+                context = '","'.join(context)
+                context = f'["{context}"]'
+                cmd = shlex.split(
+                    os.environ["DUMP_FAILURES_EDIT_COMMAND"].format(
+                        test=os.path.basename(test), manifest=manifest, context=context
+                    )
+                )
+                subprocess.run(cmd)
+
+
+def cli(args=sys.argv[1:]):
+    parser = ArgumentParser()
+    parser.add_argument("branch", help="Branch of push")
+    parser.add_argument("rev", help="Head revision of push")
+    args = parser.parse_args(args)
+
+    dump_failures(
+        args.branch,
+        args.rev,
+    )
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(cli())
---
+++ test_management_tools/test_triage_bug_filer.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python
+
+"""
+This script automates the process of filing bugs for tests skipped due to new
+or migrating platforms.
+
+It requires a bit of setup to use:
+
+1. Run `pip install -r requirements.txt` (preferably in a virutalenv)
+
+2. Ensure you can login via the bugzilla client. First add:
+
+    [DEFAULT]
+    url = https://bugzilla.mozilla.org
+
+to ~/.config/python-bugzilla/bugzillarc. Next generate a new API key from
+your Bugzilla profile and paste it into the prompt you get from:
+
+    $ bugzilla login --api-key`.
+
+If you'd like to test the tool out, use `https://bugzilla.allizom.org` as the
+url instead and request a test account from #bmo in slack.
+
+3. Set the `GECKO` environment variable to your mozilla-central clone.
+
+4. Craft the patch that disables tests. You can optionally use `{bug}` as a
+placeholder for the bug id, or {reason} as a placeholder for the reason string.
+E.g:
+
+    skip-if =
+      os == "linux" && debug  # {bug} - {reason}
+
+
+5. Export the patch to a file:
+
+    $ hg export . > orig.patch
+
+6. Run the tool. You'll need to supply the patch file, a reason string and an
+optional try url. Also consider redirecting the output to a new file:
+
+    $ ./test-triage-bug-filer path/to/orig.patch "new platform triage for foo" --try-url <try push url> > new.patch
+
+7. Import the new patch and prune the old one:
+
+    $ hg import new.patch
+    $ hg prune -r <old revision>
+"""
+
+
+import json
+import traceback
+import os
+import subprocess
+import sys
+from argparse import ArgumentParser
+from itertools import chain
+
+import bugzilla
+from unidiff import PatchSet
+
+bzapi = bugzilla.Bugzilla("https://bugzilla.mozilla.org")
+if not bzapi.logged_in:
+    bzapi.interactive_login()
+
+
+BUG_SUMMARY_TEMPLATE = "Tests skipped in '{path}' for {reason}"
+BUG_DESCRIPTION_TEMPLATE = """
+Note: This bug was filed automatically [via script](https://hg.mozilla.org/build/braindump/file/tip/test-related/test-triage-bug-filer).
+
+The following tests are being disabled in [{path}](https://searchfox.org/mozilla-central/source/{path}) due to {reason}:
+{skipped_tests}
+
+Full diff:
+```diff
+{diff}
+```
+{try_blurb}
+### Disclaimer
+Adding new platforms is not an exact science, and in order to get to green and
+enable coverage ASAP, we often err on the side of disabling when in doubt. For
+this reason, it's possible that the annotation was added in error, is covered
+by an existing intermittent, or was fixed sometime between now and when the
+annotation was made.
+
+If you believe this is the case here, please feel free to remove the
+annotation. Sorry for the inconvenience and thanks for understanding.
+""".lstrip()
+
+TRY_BLURB_TEMPLATE = """
+{try_url}
+To run these failures in your own try push, first ensure the patches from bug {bug_id}
+have landed, revert the `skip-if` annotations, then run:
+```bash
+$ ./mach try fuzzy --rebuild 3 {path}
+```
+
+Finally use the [fuzzy interface](https://firefox-source-docs.mozilla.org/tools/try/selectors/fuzzy.html) to select the task(s) which are relevant to the
+`skip-if` expression(s).
+"""
+
+TRY_URL_TEMPLATE = """
+See this [try push]({try_url}) for failures. If failures are missing, they were
+either discovered on a subsequent try push or this bug is invalid.
+"""
+
+
+def get_bug_components(paths):
+    proc = subprocess.run(
+        ["mach", "file-info", "bugzilla-component", "--format=json"] + paths,
+        capture_output=True,
+        text=True,
+        check=True,
+        cwd=os.environ["GECKO"],
+    )
+    return json.loads(proc.stdout)
+
+
+def get_skipped_tests(manifest):
+    skipped_tests = set()
+    for hunk in manifest:
+        last_test = "unknown"
+        last_key = None
+        for line in hunk:
+            if line.value.startswith("["):
+                last_test = line.value.strip().strip("[]")
+            elif not line.value.startswith(" ") and "=" in line.value:
+                last_key = line.value[: line.value.index("=")].strip()
+
+            if not line.is_added or "{bug}" not in line.value:
+                continue
+
+            if last_key == "skip-if":
+                skipped_tests.add(last_test)
+    return skipped_tests
+
+
+def create_bug(
+    product,
+    component,
+    summary,
+    description,
+    depends_on,
+    bugtype="task",
+    version="unspecified",
+    dry_run=False,
+    **kwargs,
+):
+    if dry_run:
+        description = "  " + "\n    ".join(description.splitlines())
+        print(
+            f"""
+The following bug would be filed:
+  {summary}
+  Product: {product}
+  Component: {component}
+  Depends on: {depends_on}
+  Description:
+  {description}
+"""
+        )
+        return
+
+    createinfo = bzapi.build_createbug(
+        product=product,
+        component=component,
+        summary=summary,
+        description=description,
+        version=version,
+        depends_on=depends_on,
+    )
+    createinfo["type"] = bugtype
+    createinfo.update(kwargs)
+    try:
+        bug = bzapi.createbug(createinfo)
+        print(bug.weburl, file=sys.stderr)
+        return bug.id
+    except:
+        print("Failed to create bug:", file=sys.stderr)
+        print(traceback.format_exc(), file=sys.stderr)
+
+
+def process_diff(
+    diff: str, reason: str, depends_on: str, try_url: str = "", dry_run: bool = False
+):
+    try_url = try_url or ""
+
+    patch = PatchSet.from_filename(diff, encoding="utf-8")
+    components = get_bug_components([m.path for m in patch])
+
+    for manifest in patch:
+        if (
+            not manifest.path.endswith(".ini")
+            or "testing/web-platform" in manifest.path
+        ):
+            continue
+
+        skipped_tests = get_skipped_tests(manifest)
+        if not skipped_tests:
+            continue
+
+        product, component = components[manifest.path]
+        summary = BUG_SUMMARY_TEMPLATE.format(path=manifest.path, reason=reason)
+
+        skipped_tests_str = "* " + "\n* ".join(skipped_tests)
+        if try_url:
+            url = TRY_URL_TEMPLATE.format(
+                try_url=f"{try_url}&test_paths={manifest.path}"
+            )
+        try_blurb = TRY_BLURB_TEMPLATE.format(path=manifest.path, try_url=url, bug_id=depends_on)
+        description = BUG_DESCRIPTION_TEMPLATE.format(
+            path=manifest.path,
+            reason=reason,
+            skipped_tests=skipped_tests_str,
+            diff=str(manifest),
+            try_blurb=try_blurb,
+        )
+        bug_id = create_bug(
+            product, component, summary, description, depends_on, dry_run=dry_run
+        )
+        if not bug_id:
+            continue
+
+        for line in chain.from_iterable([hunk for hunk in manifest]):
+            if line.is_added:
+                line.value = line.value.replace("{bug}", f"Bug {bug_id}")
+                line.value = line.value.replace("{reason}", reason)
+
+    if not dry_run:
+        print(patch)
+
+
+def cli(args=sys.argv[1:]):
+    parser = ArgumentParser()
+    parser.add_argument("diff", help="Path to diff that skips tests")
+    parser.add_argument("--reason", help="Reason tests are being skipped")
+    parser.add_argument(
+        "--bug-id", help="Bugzilla ID of the bug that will be landing the changes."
+    )
+    parser.add_argument(
+        "--try-url", default=None, help="URL to try push demonstrating failures"
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        default=False,
+        help="Don't submit bugs, instead print a string representation",
+    )
+    args = parser.parse_args(args)
+
+    process_diff(
+        args.diff,
+        args.reason,
+        args.bug_id,
+        try_url=args.try_url,
+        dry_run=args.dry_run,
+    )
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(cli())
