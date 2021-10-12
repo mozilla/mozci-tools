37f1a7e486f72582bc8a15aff2f9a2ca87d63bfc
Andrew Halberstadt <ahal@pm.me>
2021-10-12 10:21:32 -0400

    Remove duplicate copies of command definitions

--- citools/console/application.py
+++ citools/console/application.py
@@ -1,75 +1,9 @@
 import sys
 
-from cleo import Application, Command
+from cleo import Application
 
-from citools import test_triage_bug_filer
-from citools.dump_failures import dump_failures
-
-
-class PushFailuresCommand(Command):
-    """
-    Display failing tests for a given push.
-
-    failures
-    """
-
-    def handle(self):
-        dump_failures(
-            self.argument("branch"),
-            self.argument("rev"),
-        )
-
-
-class PushCommands(Command):
-    """
-    Contains commands that operate on a single push.
-
-    push
-        {branch : Branch the push belongs to (e.g autoland, try, etc).}
-        {rev : Head revision of the push.}
-    """
-
-    commands = [PushFailuresCommand()]
-
-    def handle(self):
-        return self.call("help", self._config.name)
-
-
-class FileBugsCommand(Command):
-    """
-    Files bugs for tests skipped due to new or migrating platforms.
-
-    file-bugs
-        {diff : Path to diff that skips tests}
-        {--reason= : Reason tests are being skipped.}
-        {--bug-id= : Bugzilla ID of the bug that will be landing the changes.}
-        {--try-url= : URL to try push demonstrating failures.}
-        {--dry-run : Don't submit bugs, instead print a string representation.}
-    """
-
-    help = test_triage_bug_filer.__doc__
-
-    def handle(self):
-        test_triage_bug_filer.process_diff(
-            self.argument("diff"),
-            self.option("reason"),
-            self.option("bug-id"),
-            try_url=self.option("try-url"),
-            dry_run=self.option("dry-run"),
-        )
-
-
-class MigrationCommands(Command):
-    """
-    Contains commands related to performing test migrations.
-
-    migration
-    """
-
-    commands = [FileBugsCommand()]
-
-    def handle(self):
-        return self.call("help", self._config.name)
+from citools.console.commands.migration import MigrationCommands
+from citools.console.commands.push import PushCommands
 
 
 def cli():
